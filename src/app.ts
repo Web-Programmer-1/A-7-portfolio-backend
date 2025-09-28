255820dd39fc218ffd53000224a6388b8b8d6dbd
tecnology-expart <mdhakimshorkar123@gmail.com>
2025-09-28 09:05:29 +0600

    addedsome

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,30 @@
+
 import compression from "compression";
 import cors from "cors";
 import express from "express";
+import globalErrorHandler from "./utils/globalErrorHandeler";
+
 
 const app = express();
 
-// Middleware
-app.use(cors()); // Enables Cross-Origin Resource Sharing
-app.use(compression()); // Compresses response bodies for faster delivery
-app.use(express.json()); // Parse incoming JSON requests
+app.use(cors({ origin: "*" }));   
+app.use(compression());
+app.use(express.json());
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
 
-// Default route for testing
+
 app.get("/", (_req, res) => {
   res.send("API is running");
 });
 
 
-// 404 Handler
-app.use((req, res, next) => {
+app.use((req, res) => {
   res.status(404).json({
     success: false,
     message: "Route Not Found",
   });
 });
+
+app.use(globalErrorHandler);
 
 export default app;
